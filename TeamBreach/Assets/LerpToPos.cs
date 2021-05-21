343146d0190566d45ab663927b81994d73da8517
Bradley Marden <bradleymarden@ez-gamelogic.com>
2021-05-21 12:01:49 +0100

    Added enemy ai

---
+++ TeamBreach/Assets/LerpToPos.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LerpToPos : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Transform Point1;
+    public Transform Point2;
+    public float speed;
+    public bool randomTime = false;
+    float rSpeed;
+    void Start()
+    {
+        transform.position = Point1.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, Point2.position , speed * Time.deltaTime);
+        if (Vector2.Distance(transform.position, Point2.position) <=1)
+        {
+           transform.position = Point1.transform.position;
+        }
+    }
+}
--- TeamBreach/Assets/Scripts/OnTrigger.cs
+++ TeamBreach/Assets/Scripts/OnTrigger.cs
@@ -11,10 +11,46 @@ public class OnTrigger : MonoBehaviour
         {
             
             collision.transform.GetComponent<PlayerController>().HasCollidedWithWall(transform.gameObject);
-            collision.transform.GetComponent<PlayerController>().m_IsVolatile = true;
-            collision.transform.GetComponent<PlayerController>().SetVolatile();
+            if (transform.gameObject.tag != "Enemy")
+            {
+
+                collision.transform.GetComponent<PlayerController>().m_IsVolatile = true;
+                collision.transform.GetComponent<PlayerController>().SetVolatile();
+            }
+            
             //Destroy(transform.gameObject);
         }
     }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.transform.tag == "Player")
+        {
 
+            collision.transform.GetComponent<PlayerController>().HasCollidedWithWall(transform.gameObject);
+            if (transform.gameObject.tag != "Enemy")
+            {
+
+                collision.transform.GetComponent<PlayerController>().m_IsVolatile = true;
+                collision.transform.GetComponent<PlayerController>().SetVolatile();
+            }
+
+            //Destroy(transform.gameObject);
+        }
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.transform.tag == "Player")
+        {
+
+            collision.transform.GetComponent<PlayerController>().HasCollidedWithWall(transform.gameObject);
+            if (transform.gameObject.tag != "Enemy")
+            {
+
+                collision.transform.GetComponent<PlayerController>().m_IsVolatile = true;
+                collision.transform.GetComponent<PlayerController>().SetVolatile();
+            }
+
+            //Destroy(transform.gameObject);
+        }
+    }
 }
