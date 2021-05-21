cff3aa2ec56441354b02084c28ce4678fbbf9e5f
Bradley Marden <bradleymarden@ez-gamelogic.com>
2021-05-21 09:37:05 +0100

    Gas

--- TeamBreach/Assets/gasTrigger.cs
+++ TeamBreach/Assets/gasTrigger.cs
@@ -15,4 +15,18 @@ public class gasTrigger : MonoBehaviour
     {
         
     }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.tag == "Player")
+        {
+
+            if (collision.transform.GetComponent<PlayerController>().m_PlayerState == PlayerController.PlayerState.FAST ||
+                collision.transform.GetComponent<PlayerController>().m_PlayerState == PlayerController.PlayerState.VOLATILE ||
+                collision.transform.GetComponent<PlayerController>().m_PlayerState == PlayerController.PlayerState.DEFAULT)
+            {
+                Debug.LogError("State" + collision.transform.GetComponent<PlayerController>().m_PlayerState);
+                collision.transform.GetComponent<PlayerController>().Extinguish();
+            }
+        }
+    }
 }
