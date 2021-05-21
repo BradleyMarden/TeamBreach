3444ea433594dbdb285bb2a2d515327cc011cf4a
Bradley Marden <bradleymarden@ez-gamelogic.com>
2021-05-21 16:09:42 +0100

    Added Key and wall

---
+++ TeamBreach/Assets/OpenWithKEy.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenWithKEy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.tag == "Player")
+        {
+
+            if (collision.transform.GetComponent<PlayerController>().hasKey)
+            {
+                collision.transform.GetComponent<PlayerController>().BreakThroughWall();
+                transform.gameObject.SetActive(false);
+
+            }
+        }
+    }
+}
